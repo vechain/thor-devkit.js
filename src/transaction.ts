e189a035259efe2513124dce31a1d88cc55cbac5
tony <liboliqi@gmail.com>
2025-05-07 16:01:14 +0800

    add support of dynamic fee transaction

--- src/transaction.ts
+++ src/transaction.ts
@@ -13,15 +13,38 @@ export class Transaction {
      * @param unsigned to indicator if the encoded buffer contains signature
      */
     public static decode(raw: Buffer, unsigned?: boolean) {
+        let type: Transaction.Type
+        if (raw.length > 0 && raw[0] > 0x7f) {
+            type = Transaction.Type.Legacy
+        } else {
+            if (raw.length === 0) {
+                throw new Error('typed transaction too short')
+            }
+            if (raw[0] === Transaction.Type.DynamicFee) {
+                type = Transaction.Type.DynamicFee
+                // remove type identifier for subsequent decoding
+                raw = raw.slice(1)
+            } else {
+                throw new Error('transaction type not supported: ' + raw[0])
+            }
+        }
+
         let body: Transaction.Body
         let signature: Buffer | undefined
         if (unsigned) {
-            body = unsignedTxRLP.decode(raw)
+            body = type === Transaction.Type.DynamicFee ?
+                unsignedDynamicFeeTxRLP.decode(raw) :
+                unsignedLegacyTxRLP.decode(raw)
+            body.type = type
         } else {
-            const decoded = txRLP.decode(raw)
+            const decoded = type === Transaction.Type.DynamicFee ?
+                dynamicFeeTxRLP.decode(raw) :
+                legacyTxRLP.decode(raw)
+
             signature = decoded.signature as Buffer
             delete decoded.signature
             body = decoded
+            body.type = type
         }
 
         const reserved = body.reserved as Buffer[]
@@ -82,16 +105,32 @@ export class Transaction {
         }
     }
 
+    /** returns transaction type, type legacy will return if type is not set */
+    get type() {
+        if (this.body.type && this.body.type === Transaction.Type.DynamicFee) {
+            return Transaction.Type.DynamicFee
+        }
+        return Transaction.Type.Legacy
+    }
+
     /**
      * compute signing hashes.
      * It returns tx hash for origin or delegator depends on param `delegateFor`.
      * @param delegateFor address of intended tx origin. If set, the returned hash is for delegator to sign.
      */
     public signingHash(delegateFor?: string) {
-        const reserved = this._encodeReserved()
-        const buf = unsignedTxRLP.encode({ ...this.body, reserved })
-        const hash = blake2b256(buf)
+        this.checkType()
 
+        const reserved = this._encodeReserved()
+        let buf: Buffer
+        if (this.type === Transaction.Type.DynamicFee) {
+            const raw = unsignedDynamicFeeTxRLP.encode({ ...this.body, reserved })
+            buf = Buffer.concat([Buffer.from([Transaction.Type.DynamicFee]), raw])
+        } else {
+            buf = unsignedLegacyTxRLP.encode({ ...this.body, reserved })
+        }
+
+        const hash = blake2b256(buf)
         if (delegateFor) {
             if (!/^0x[0-9a-f]{40}$/i.test(delegateFor)) {
                 throw new Error('delegateFor expected address')
@@ -152,12 +191,19 @@ export class Transaction {
 
     /** encode into Buffer */
     public encode() {
+        this.checkType()
         const reserved = this._encodeReserved()
-        if (this.signature) {
-            return txRLP.encode({ ...this.body, reserved, signature: this.signature })
+
+        if (this.type === Transaction.Type.DynamicFee) {
+            const raw = this.signature ?
+                dynamicFeeTxRLP.encode({ ...this.body, reserved, signature: this.signature }) :
+                unsignedDynamicFeeTxRLP.encode({ ...this.body, reserved })
+            return Buffer.concat([Buffer.from([Transaction.Type.DynamicFee]), raw])
         }
 
-        return unsignedTxRLP.encode({ ...this.body, reserved })
+        return this.signature ?
+            legacyTxRLP.encode({ ...this.body, reserved, signature: this.signature }) :
+            unsignedLegacyTxRLP.encode({ ...this.body, reserved })
     }
 
     private _encodeReserved() {
@@ -180,6 +226,14 @@ export class Transaction {
         const expectedSigLen = this.delegated ? 65 * 2 : 65
         return this.signature ? this.signature.length === expectedSigLen : false
     }
+
+    private checkType() {
+        // allow type to be undefined to be compatible with older spec
+        if (this.body.hasOwnProperty('type') && this.body.type !== undefined &&
+            this.body.type !== Transaction.Type.Legacy && this.body.type !== Transaction.Type.DynamicFee) {
+            throw new Error('unsupported transaction type: ' + this.body.type)
+        }
+    }
 }
 
 export namespace Transaction {
@@ -198,8 +252,16 @@ export namespace Transaction {
         data: string
     }
 
-    /** body type */
-    export interface Body {
+    export enum Type {
+        Legacy = 0,
+        DynamicFee = 81,
+    }
+
+    export type Body = LegacyBody | DynamicFeeBody
+
+    /** legacy transaction body type */
+    export interface LegacyBody {
+        type?: Type.Legacy
         /** last byte of genesis block ID */
         chainTag: number
         /** 8 bytes prefix of some block's ID */
@@ -210,6 +272,35 @@ export namespace Transaction {
         clauses: Clause[]
         /** coef applied to base gas price [0,255] */
         gasPriceCoef: number
+        /** max gas provided for execution */
+        gas: string | number
+        /** ID of another tx that is depended */
+        dependsOn: string | null
+        /** nonce value for various purposes */
+        nonce: string | number
+
+        reserved?: {
+            /** tx feature bits */
+            features?: number
+            unused?: Buffer[]
+        }
+    }
+
+    /** dynamic fee transaction body type */
+    export interface DynamicFeeBody {
+        type: Type.DynamicFee
+        /** last byte of genesis block ID */
+        chainTag: number
+        /** 8 bytes prefix of some block's ID */
+        blockRef: string
+        /** constraint of time bucket */
+        expiration: number
+        /** array of clauses */
+        clauses: Clause[]
+        /** max priority fee per gas */
+        maxPriorityFeePerGas: string | number
+        /** max fee per gas */
+        maxFeePerGas: string | number
         /** max gas provided for execution */
         gas: string | number
         /** ID of another tx that is depended */
@@ -264,8 +355,8 @@ export namespace Transaction {
     }
 }
 
-const unsignedTxRLP = new RLP({
-    name: 'tx',
+const unsignedLegacyTxRLP = new RLP({
+    name: 'unsigned legacy tx',
     kind: [
         { name: 'chainTag', kind: new RLP.NumericKind(1) },
         { name: 'blockRef', kind: new RLP.CompactFixedBlobKind(8) },
@@ -287,9 +378,39 @@ const unsignedTxRLP = new RLP({
     ],
 })
 
-const txRLP = new RLP({
-    name: 'tx',
-    kind: [...(unsignedTxRLP.profile.kind as RLP.Profile[]), { name: 'signature', kind: new RLP.BufferKind() }],
+const legacyTxRLP = new RLP({
+    name: 'legacy tx',
+    kind: [...(unsignedLegacyTxRLP.profile.kind as RLP.Profile[]), { name: 'signature', kind: new RLP.BufferKind() }],
+})
+
+const unsignedDynamicFeeTxRLP = new RLP({
+    name: 'unsigned dynamic fee tx',
+    kind: [
+        { name: 'chainTag', kind: new RLP.NumericKind(1) },
+        { name: 'blockRef', kind: new RLP.CompactFixedBlobKind(8) },
+        { name: 'expiration', kind: new RLP.NumericKind(4) },
+        {
+            name: 'clauses', kind: {
+                item: [
+                    { name: 'to', kind: new RLP.NullableFixedBlobKind(20) },
+                    { name: 'value', kind: new RLP.NumericKind(32) },
+                    { name: 'data', kind: new RLP.BlobKind() },
+                ],
+            },
+        },
+        { name: 'maxPriorityFeePerGas', kind: new RLP.NumericKind(32) },
+        { name: 'maxFeePerGas', kind: new RLP.NumericKind(32) },
+        { name: 'gas', kind: new RLP.NumericKind(8) },
+        { name: 'dependsOn', kind: new RLP.NullableFixedBlobKind(32) },
+        { name: 'nonce', kind: new RLP.NumericKind(8) },
+        { name: 'reserved', kind: { item: new RLP.BufferKind() } },
+    ],
+})
+
+const dynamicFeeTxRLP = new RLP({
+    name: 'dynamic fee tx',
+    // tslint:disable-next-line:max-line-length
+    kind: [...(unsignedDynamicFeeTxRLP.profile.kind as RLP.Profile[]), { name: 'signature', kind: new RLP.BufferKind() }],
 })
 
 const featuresKind = new RLP.NumericKind(4)
